9f188eda8422fd75c969e572b5a497dd5e0d507c
육승준 <6seungjun@naver.com>
2021-05-10 13:46:05 +0900

    first 3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="yuk yuk yuk!"/>
   </div>
 </template>
 
